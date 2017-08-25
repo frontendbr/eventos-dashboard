13f96a283021e3149236c096d5bf74a4745b8a72
Rogerio Angeliski <angeliski@hotmail.com>
2017-08-24 22:23:54 -0300

    Iniciando

---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>Bem Vindo!</h1>
+        <h1>{{ msg }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js PWA',
+    };
+  },
+};
+</script>
+
+<style>
+
+</style>
