a6a3aacaf653a3521ae464ded1b792f87acbb983
Marcel Zanluca <marcel.zanluca@gmail.com>
2018-04-25 22:32:11 -0300

    Removed unnecessary file and fixed wrong files name

--- src/screens/login/index.js
+++ src/screens/login/index.js
@@ -1,2 +1,20 @@
-import Login from './Login'
+import React from 'react'
+import styled from 'styled-components'
+
+import AuthenticationPanel from './authentication-panel'
+import IssuePanel from './issue-panel'
+
+const StyledPage = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+`
+
+const Login = () => (
+  <StyledPage>
+    <AuthenticationPanel />
+    <IssuePanel />
+  </StyledPage>
+)
+
 export default Login
