04b9bab2dfcdb3e235ed4638f9b704927220f584
Marcel Zanluca <marcel.zanluca@gmail.com>
2018-04-25 22:37:02 -0300

    Removido arquivos desnecessarios

--- src/components/github-button/index.js
+++ src/components/github-button/index.js
@@ -1,2 +1,50 @@
-import GithubButton from './GithubButton'
-export default GithubButton
+import React from 'react'
+import { string, bool } from 'prop-types'
+import styled from 'styled-components'
+import { FaGithub } from 'react-icons/lib/fa'
+
+const StyledButton = styled.button`
+  display: flex;
+  cursor: pointer;
+  align-items: center;
+  justify-content: center;
+  padding: 0;
+  border: 0;
+  border-radius: 5px;
+  overflow: hidden;
+  max-width: 488px;
+  background-color: ${p => (p.primary ? '#0081E2' : '#fff')};
+  color: ${p => p.primary ? '#fff' : '#2d2d2d'};
+
+  > span {
+    flex-grow: 1;
+    text-transform: uppercase;
+    font-size: 1rem;
+  }
+`
+
+const StyledFaGithub = styled(FaGithub)`
+  font-size: 3rem;
+  background-color: ${p => (p.primary === 'true' ? '#0072C8' : '#2d2d2d')};
+  padding: 6px;
+  color: #fff;
+`
+
+const GitHubButton = ({ children, primary }) => (
+  <StyledButton primary={primary}>
+    <StyledFaGithub primary={primary.toString()} />
+    <span>{children}</span>
+  </StyledButton>
+)
+
+GitHubButton.propTypes = {
+  children: string,
+  primary: bool
+}
+
+GitHubButton.defaultProps = {
+  children: 'Github',
+  primary: false
+}
+
+export default GitHubButton
--- src/components/logo/index.js
+++ src/components/logo/index.js
@@ -1,2 +1,18 @@
-import Logo from './Logo'
+import React from 'react'
+import styled from 'styled-components'
+
+import LogoSVG from './logo-600px--color.svg'
+
+const StyledLogo = styled.img`
+  width: 200px;
+  height: 200px;
+`
+
+const Logo = ({ ...props }) => (
+  <StyledLogo
+    src={LogoSVG} alt='FrontEndBR'
+    {...props}
+  />
+)
+
 export default Logo
